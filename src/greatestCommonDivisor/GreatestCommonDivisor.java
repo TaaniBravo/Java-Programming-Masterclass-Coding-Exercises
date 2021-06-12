7e03478195442b54bf0413bb27626eb19d3575cd
Taani Maama <taani1maama@live.com>
2021-06-11 21:18:41 -0700

    Finished the exercise for the GreatestCommonDivisor.

---
+++ src/greatestCommonDivisor/GreatestCommonDivisor.java
@@ -0,0 +1,25 @@
+package greatestCommonDivisor;
+
+public class GreatestCommonDivisor {
+
+    public static int getGreatestCommonDivisor (int first, int second) {
+        if (first < 10 || second < 10) return -1;
+
+        while (first != second) {
+            if (first > second) {
+                first -= second;
+            } else {
+                second -= first;
+            }
+        }
+
+        return Math.max(first, second);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getGreatestCommonDivisor(25, 15));
+        System.out.println(getGreatestCommonDivisor(12, 30));
+        System.out.println(getGreatestCommonDivisor(9, 18));
+        System.out.println(getGreatestCommonDivisor(81, 153));
+    }
+}
